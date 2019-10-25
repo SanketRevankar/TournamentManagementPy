b32be47737622164d0b49f65ae36ce960d545f1b
Sanket Revankar <26522131+sanketrevankar@users.noreply.github.com>
2019-10-25 22:49:51 +0530

    Updated passive mode for FTP
    Signed-off-by: Sanket Revankar <26522131+SanketRevankar@users.noreply.github.com>

---
+++ DownloadLogsFromFTP/main.py
@@ -0,0 +1,105 @@
+import datetime
+import json
+import os
+from ftplib import FTP
+
+from google.cloud import storage
+from google.cloud.storage import Blob
+
+
+def get_logs_from_ftp(request):
+    """
+    Get log data from instance
+
+    :param date: date for which the logs will be downloaded
+    :param server_id: Id of the server
+    :param folder: Destination folder
+    """
+
+    request_json = request.get_json(silent=True)
+
+    score_starting_ = request_json['request_json']
+    logs_starting_ = request_json['logs_starting_']
+    folder = request_json['folder']
+    results_ = request_json['results_']
+    amxmodx_logs_ = request_json['amxmodx_logs_']
+    cstrike_logs_ = request_json['cstrike_logs_']
+    ip = request_json['ip']
+    username = request_json['username']
+    password = request_json['password']
+    date = request_json['date']
+
+    folders = [
+        [results_, score_starting_ + folder],
+        [amxmodx_logs_, logs_starting_ + folder],
+        [cstrike_logs_, logs_starting_ + folder],
+    ]
+    ftp = get_ftp_connection(ip, username, password)
+
+    for c_folder in folders:
+        for file in ftp.mlsd(c_folder[0]):
+            if file[1]['type'] == 'dir' or (".log" not in file[0] and ".txt" not in file[0]):
+                continue
+            date_file = datetime.datetime.strptime(file[1]['modify'], "%Y%m%d%H%M%S")
+
+            if date_file.astimezone() >= date:
+                source = c_folder[0] + "/" + file[0]
+                destination = c_folder[1] + "/" + file[0]
+                temp_dest = '/tmp/' + file[0]
+                download(ftp, source, temp_dest)
+                upload_file(destination, temp_dest)
+                os.remove(temp_dest)
+
+    ftp.close()
+
+def get_ftp_connection(ip, username, password):
+    """
+    Get a connection for server with server Id from ServerList
+    """
+
+    ftp = FTP()
+    ftp.connect(ip, 21)
+    ftp.login(username, password)
+    ftp.set_pasv(False)
+
+    return ftp
+
+
+def download(ftp, src, des):
+    """
+    Download file from FTP to local
+
+    :param ftp: FTP connection var
+    :param src: Source path - Cloud
+    :param des: Destination path - local
+    """
+
+    f = open(des, 'wb')
+    ftp.retrbinary("RETR " + src, f.write)
+
+
+def upload_file(file_name, file_path):
+    """
+    Upload a file from local storage to bucket
+
+    :param file_name: Name of the file to upload. (Should include path from bucket)
+    :param file_path: Path of the file in local system
+    """
+
+    client = storage.Client()
+    bucket_name = 'ncl'
+    bucket = client.get_bucket(bucket_name)
+
+    blob = Blob(file_name, bucket)
+    with open(file_path, "rb") as my_file:
+        blob.upload_from_file(my_file)
+    log_it_storage(file_name, 'file')
+
+def log_it_storage(location, file_type):
+    log_ = {
+        'location': location,
+        'type': 'storage',
+        'file_type': file_type
+    }
+    dumps = json.dumps(log_)
+    print(dumps)
--- Helpers/FTPHelper.py
+++ Helpers/FTPHelper.py
@@ -1,6 +1,9 @@
 import datetime
+import json
 import os
 from ftplib import FTP
+
+import requests
 
 from TournamentManagementPy import handler
 from constants import StringConstants as sC, PyConstants as pC
@@ -22,8 +25,22 @@ class FTPHelper:
         self.amxmodx_logs_ = config[sC.FOLDER_LOCATIONS][sC.ADDONS_AMXMODX_LOGS]
         self.cstrike_logs_ = config[sC.FOLDER_LOCATIONS][sC.CSTRIKE_LOGS]
 
+        _old_makepasv = FTP.makepasv
+
         print('{} - Initialized'.format(__name__))
 
+        def _new_makepasv(self_):
+            """
+            To use passive mode for FTP
+
+            :param self_: current reference
+            :return: host and port
+            """
+            host, port = _old_makepasv(self_)
+            host = self_.sock.getpeername()[0]
+            return host, port
+
+        FTP.makepasv = _new_makepasv
 
     @staticmethod
     def get_ftp_connection(server_id):
@@ -33,9 +50,8 @@ class FTPHelper:
         :param server_id: Id of the server as mentioned in ServerList
         :return:
         """
-        ip = ServerList[server_id][sC.SERVER_IP]
-        # node = handler.cloudServerHelper.util.get_node(ServerList[server_id][sC.INSTANCE_NAME])
-        # ip = handler.cloudServerHelper.util.ip(node)
+        node = handler.cloudServerHelper.util.get_node(ServerList[server_id][sC.INSTANCE_NAME])
+        ip = handler.cloudServerHelper.util.ip(node)
 
         ftp = FTP()
         ftp.connect(ip, 21)
@@ -48,7 +64,7 @@ class FTPHelper:
     def close_ftp_connection(ftp):
         ftp.close()
 
-    def download(self, server_id, src, des):
+    def download(self, ftp, src, des):
         """
         Download file from FTP to local
 
@@ -56,13 +72,9 @@ class FTPHelper:
         :param src: Source path - Cloud
         :param des: Destination path - local
         """
-        ftp = self.get_ftp_connection(server_id)
-
         f = open(des, sC.WB_MODE)
         ftp.retrbinary(sC.RETR_ + src, f.write)
         self.close_ftp_connection(f)
-
-        self.close_ftp_connection(ftp)
 
     def get_hltv_demos_from_ftp(self, date, server_id, folder):
         """
@@ -73,23 +85,21 @@ class FTPHelper:
         :param folder: Destination folder
         """
         ftp = self.get_ftp_connection(server_id)
-        files = []
+
         for file in ftp.mlsd(sC.CSTRIKE):
             if file[1][sC.TYPE] == sC.DIR or sC.DEMO_FORMAT not in file[0]:
                 continue
-            files.append(file)
-        self.close_ftp_connection(ftp)
-
-        for file in files:
             date_file = datetime.datetime.strptime(file[1][sC.MODIFY], pC.DATETIME_FORMAT)
 
             if date_file.astimezone() >= date:
                 source = sC.CSTRIKE + sC.SEPARATOR + file[0]
                 destination = self.locations_hltv_starting_ + folder + sC.SEPARATOR + file[0]
                 temp_dest = self.temp + file[0]
-                self.download(server_id, source, temp_dest)
+                self.download(ftp, source, temp_dest)
                 handler.cloudStorageHelper.upload_file(destination, temp_dest)
                 os.remove(temp_dest)
+
+        self.close_ftp_connection(ftp)
 
     def get_logs_from_ftp(self, date, server_id, folder):
         """
@@ -99,29 +109,47 @@ class FTPHelper:
         :param server_id: Id of the server
         :param folder: Destination folder
         """
+        # ftp = self.get_ftp_connection(server_id)
+        #
+        # folders = [
+        #     [self.results_, self.score_starting_ + folder],
+        #     [self.amxmodx_logs_, self.logs_starting_ + folder],
+        #     [self.cstrike_logs_, self.logs_starting_ + folder],
+        # ]
+        #
 
-        folders = [
-            [self.results_, self.score_starting_ + folder],
-            [self.amxmodx_logs_, self.logs_starting_ + folder],
-            [self.cstrike_logs_, self.logs_starting_ + folder],
-        ]
+        # for c_folder in folders:
+        #     for file in ftp.mlsd(c_folder[0]):
+        #         if file[1][sC.TYPE] == sC.DIR or (sC.LOG not in file[0] and sC.TXT not in file[0]):
+        #             continue
+        #         date_file = datetime.datetime.strptime(file[1][sC.MODIFY], pC.DATETIME_FORMAT)
+        #
+        #         if date_file.astimezone() >= date:
+        #             source = c_folder[0] + sC.SEPARATOR + file[0]
+        #             destination = c_folder[1] + sC.SEPARATOR + file[0]
+        #             temp_dest = self.temp + file[0]
+        #             self.download(ftp, source, temp_dest)
+        #             handler.cloudStorageHelper.upload_file(destination, temp_dest)
+        #             os.remove(temp_dest)
+        #
+        # self.close_ftp_connection(ftp)
 
-        for c_folder in folders:
-            ftp = self.get_ftp_connection(server_id)
-            files = []
-            for file in ftp.mlsd(c_folder[0]):
-                if file[1][sC.TYPE] == sC.DIR or (sC.LOG not in file[0] and sC.TXT not in file[0]):
-                    continue
-                files.append(file)
-            self.close_ftp_connection(ftp)
+        node = handler.cloudServerHelper.util.get_node(ServerList[server_id][sC.INSTANCE_NAME])
+        ip = handler.cloudServerHelper.util.ip(node)
 
-            for file in files:
-                date_file = datetime.datetime.strptime(file[1][sC.MODIFY], pC.DATETIME_FORMAT)
+        request = {
+            'score_starting_': self.score_starting_,
+            'logs_starting_': self.logs_starting_,
+            'folder': folder,
+            'results_': self.results_,
+            'amxmodx_logs_': self.amxmodx_logs_,
+            'cstrike_logs_': self.cstrike_logs_,
+            'ip': ip,
+            'username': ServerList[server_id][sC.USERNAME],
+            'password': ServerList[server_id][sC.PASSWORD],
+            'date': date,
+        }
 
-                if date_file.astimezone() >= date:
-                    source = c_folder[0] + sC.SEPARATOR + file[0]
-                    destination = c_folder[1] + sC.SEPARATOR + file[0]
-                    temp_dest = self.temp + file[0]
-                    self.download(ftp, source, temp_dest)
-                    handler.cloudStorageHelper.upload_file(destination, temp_dest)
-                    os.remove(temp_dest)
+        data_json = json.dumps(request)
+        r = requests.post("https://us-central1-narcogaming.cloudfunctions.net/get_logs_from_ftp", json=data_json)
+        print(r)
