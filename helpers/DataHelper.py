11a8c706dc3a0d5937a4abcb193076b6b3c2212b
Sanket Revankar <26522131+sanketrevankar@users.noreply.github.com>
2020-01-02 10:01:50 +0530

    Fixed changed vc not reflecting in Teams
    Signed-off-by: Sanket Revankar <26522131+SanketRevankar@users.noreply.github.com>

--- helpers/DataHelper.py
+++ helpers/DataHelper.py
@@ -85,7 +85,7 @@ class DataHelper:
         :return: Captain's and V. Captains Details as a Dict Object
         """
 
-        team_data = self.get_team_data_by_id(team_id)
+        team_data = handler.fireStoreHelper.util.fsh_get_team_data_by_id(team_id)
         captain_data = self.get_player_data_by_id(team_data['captain'])
         vc_data_ = {}
         if 'vice_captain' in team_data:
@@ -252,6 +252,19 @@ class DataHelper:
         if self.mode_9:
             return PlayerList[SteamList[steam_id]]['team'] if 'team' in PlayerList[SteamList[steam_id]] else None, \
                 PlayerList[SteamList[steam_id]]['username'], PlayerList[SteamList[steam_id]]['name']
+
+    def get_ips_by_steam_id(self, steam_id):
+        """
+        Returns Ips of the player with given steam Id
+        Only Made to use in Mode 9
+
+        :param steam_id: Steam Id of the player
+        :return: Team, Username and Name
+        """
+
+        if self.mode_9:
+            return PlayerList[SteamList[steam_id]]['ips']
+
 
     @staticmethod
     def get_steam_id(index, current_team):
