272ffd9573238b82fdee5476ca486d156c25dd69
Sanket Revankar <26522131+sanketrevankar@users.noreply.github.com>
2020-01-02 09:57:22 +0530

    Fixed vc changing
    Signed-off-by: Sanket Revankar <26522131+SanketRevankar@users.noreply.github.com>

--- TeamFormation/api/Functions.py
+++ TeamFormation/api/Functions.py
@@ -61,7 +61,7 @@ def make_captain(request):
 
     handler.logHelper.log_it_api(request, __name__ + '.make_captain', target=target_id)
     team_id = handler.dataHelper.get_team_id_by_player_id(player_id)
-    handler.fireStoreHelper.util.update_document(handler.fireStoreHelper.TEAMS, team_id, {'vice_captain': target_id})
+    handler.fireStoreHelper.util.update_document(handler.fireStoreHelper.util.TEAMS, team_id, {'vice_captain': target_id})
     handler.adminHelper.add_admin(target_id)
 
     return HttpResponse('')
@@ -75,7 +75,7 @@ def remove_captain(request):
     handler.logHelper.log_it_api(request, __name__ + '.remove_captain', target=target_id)
 
     team_id = handler.dataHelper.get_team_id_by_player_id(player_id)
-    handler.fireStoreHelper.util.update_document(handler.fireStoreHelper.TEAMS, team_id,
+    handler.fireStoreHelper.util.update_document(handler.fireStoreHelper.util.TEAMS, team_id,
                                             {'vice_captain': firestore_v1.DELETE_FIELD})
     handler.adminHelper.remove_admin(target_id)
 
